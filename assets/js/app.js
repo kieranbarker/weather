b9818b0b2c9fe0c631a4f719397bd9d1ee74fb37
Kieran Barker <kieran11397@gmail.com>
2019-11-17 13:42:23 +0000

    Add favicon, store endpoints, and log location data to the console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,19 @@
 ;(function(d) {
 
+  var endpoints = {
+    location: "https://ipapi.co/json",
+    weather: {
+      url: "https://api.weatherbit.io/v2.0/current",
+      apiKey: "ee0dd94ba1d741ef95017dd656b88a52"
+    }
+  };
 
+  function getJSON(response) {
+    return (response.ok) ? response.json() : Promise.reject(response);
+  }
+
+  fetch(endpoints.location)
+    .then(getJSON)
+    .then(console.dir);
 
 })(document);
